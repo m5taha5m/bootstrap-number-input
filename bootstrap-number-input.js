d1d13c49a137b4b06b669994cf11a01a20ff4241
Taha Moghaddam <m5taha5m@gmail.com>
2017-10-22 05:47:36 +0330

    Update bootstrap-number-input.js

--- bootstrap-number-input.js
+++ bootstrap-number-input.js
@@ -65,7 +65,7 @@
 			$("<span class='input-group-btn'></span>").append(up).appendTo(group);
 
 			// remove spins from original
-			clone.prop('type', 'text').keydown(function(e) {
+			clone.prop('type', 'number').keydown(function(e) {
 				if ($.inArray(e.keyCode, [46, 8, 9, 27, 13, 110, 190]) !== -1 ||
 					(e.keyCode == 65 && e.ctrlKey === true) ||
 					(e.keyCode >= 35 && e.keyCode <= 39)) {
